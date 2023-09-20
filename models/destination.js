07571d43440b4b3c8c4b6bfcd336e637fadbe89f
veecastro <vcastro.zrt@gmail.com>
2023-09-19 20:56:54 -0400

    css

--- models/destination.js
+++ models/destination.js
@@ -8,7 +8,8 @@ const destinationSchema = new mongoose.Schema(
         name: { type: String, required: true },
         country: { type: String, required: true },
         city: { type: String, required: true },
-        // images: { type: String, required: true },
+
+        images: { type: String, required: true },
         description: { type: String, required: false },
         // name: req.body.name,
         // photo: {
